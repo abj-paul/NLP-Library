409c1b9aa42ce1b351707daf83cfe2f269fa539d
Abhijit Paul <bsse1201@iit.du.ac.bd>
2022-02-09 22:51:07 +0600

    Stuck with punctuations

--- String-Library.cpp
+++ String-Library.cpp
@@ -2,6 +2,60 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-  return 0;
+int string_len(char* x){
+  int i=0;
+  while(x[i]!='\0'){
+    i++;
+  }
+  return i;
+}
+int string_len(const char* x){
+  int i=0;
+  while(x[i]!='\0'){
+    i++;
+  }
+  return i;
+}
+
+void string_concatenate(char first[], char second[], char sentence[]){
+  int i=0;
+  while(first[i]!='\0'){
+    sentence[i] = first[i];
+    i++;
+  }
+  sentence[i]=' ';
+  i++;
+  int j=0;
+  while(second[j]!='\0'){
+    sentence[i]=second[j];
+    i++;
+    j++;
+  }
+  sentence[i]='\0';
+}
+
+bool is_same_string(const char* str,char word[]){
+  //printf("Compare Strings Function.\n");
+  int n1 = string_len(str);
+  int n2 = string_len(word);
+
+  if(n1!=n2) return false;
+  //printf("%s\n",word);
+  for(int i=0; i<n1; i++){
+    if(str[i]!=word[i]) return false;
+  }
+  return true;
+}
+
+bool is_same_string(char* str,const char* word){
+  //printf("Compare Strings Function.\n");
+  int n1 = string_len(str);
+  int n2 = string_len(word);
+
+  if(n1!=n2) return false;
+  //printf("%s\n",word);
+  for(int i=0; i<n1; i++){
+    if(str[i]!=word[i]) return false;
+  }
+  return true;
 }
---
+++ find_string_match.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include "String-Library.cpp"
+using namespace std;
+
+#define MAX_WORD_SIZE 30
+// Function Prototypes
+bool is_same_string(char* str,char* word);
+bool is_same_string(const char* str,char* word);
+int find_match(const char* str, const char* filename);
+
+int main(){
+  printf("Match found: %d\n",find_match("you have","korpus/you-have-5.txt"));
+  //printf("Match found: %d\n",find_match("you have","korpus/stanfordSentimentTreebank/datasetSentences.txt"));
+  return 0;
+}
+
+int find_match(const char* str, const char* filename){
+  //printf("Find match function.\n");
+  int count=0;
+  FILE* fptr = fopen(filename, "r");
+  if(fptr==NULL) return -1; // ERROR CODE: NO FILE FOUND
+  char two_word[MAX_WORD_SIZE*2], word1[MAX_WORD_SIZE], word2[MAX_WORD_SIZE];
+  while(!feof(fptr)){
+    fscanf(fptr,"%s %s",word1, word2);
+    string_concatenate(word1, word2, two_word);
+    printf("%s\n",two_word);
+    if(is_same_string(str,two_word)) count++;
+    int n2 = string_len(word2);
+    if(is_same_string(word2,".")) fseek(fptr, n2*(-1), SEEK_CUR);
+  }
+  return count;
+}
+
